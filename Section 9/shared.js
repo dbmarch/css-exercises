fe404bcaf3811cb7f7984808a9c3864035782483
dbmarch <dbmarch@gmail.com>
2018-08-18 22:10:09 -0500

    integrate the mobile nav across all the pages.

--- Section 9/shared.js
+++ Section 9/shared.js
@@ -7,32 +7,43 @@ console.dir(buttons);
 
 buttons.forEach(element => {
     element.addEventListener('click', () => {
-        backdrop.style.display = 'block';
-        modal.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
+        // backdrop.style.display = 'block';
+        // modal.style.display = 'block';
     })
 });
 
 
 var yesButton = document.querySelector('.modal__action');
 var noButton = document.querySelector('.modal__action--negative');
+if (yesButton) {
+    yesButton.addEventListener('click', closeModal);
+}
 
-yesButton.addEventListener('click', closeModal);
-noButton.addEventListener('click', closeModal);
+if (noButton) {
+    noButton.addEventListener('click', closeModal);
+}
 
 var hamburger = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 hamburger.addEventListener('click', () => {
-    mobileNav.style.display = "block";
-    backdrop.style.display = "block";
+    // mobileNav.style.display = "block";
+    // backdrop.style.display = "block";
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 })
 
 backdrop.addEventListener('click', () => {
-    mobileNav.style.display = "none";
+    // mobileNav.style.display = "none";
+    mobileNav.classList.remove('open');
     closeModal();
 })
 
 
 function closeModal() {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    backdrop.classList.remove('open');
+    modal.classList.remove('open');
 }
